d67c85df72a6fec78e691854298558f1bc807cd8
zhangzhzh <zhangzhzh@uinnova.com>
2020-03-20 15:23:19 +0800

    fix: 调整片段识别符

--- iframe跨域及通信问题/iframe-son2/index.js
+++ iframe跨域及通信问题/iframe-son2/index.js
@@ -7,9 +7,9 @@ window.onload = function () {
         var origin = event.origin || event.originalEvent.origin;
         if (origin === 'http://192.168.202.90:5556') {
             var data = event.data;
-            loginName = JSON.parse(data).userName;
-            loginType = JSON.parse(data).type;
-            console.log(loginName, loginType);
+            // loginName = JSON.parse(data).userName;
+            // loginType = JSON.parse(data).type;
+            // console.log(loginName, loginType);
         }
     }, false);
 }
