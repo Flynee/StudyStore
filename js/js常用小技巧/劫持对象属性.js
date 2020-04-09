558b4606e069d1b9b28cd497f621e00affdf48b8
zhangzhzh <zhangzhzh@uinnova.com>
2020-04-09 13:37:38 +0800

    fix: 更新对象属性劫持

--- js/js常用小技巧/劫持对象属性.js
+++ js/js常用小技巧/劫持对象属性.js
@@ -2,11 +2,16 @@ var obj = { visible: false };
 
 function watch(obj) {
     Object.defineProperty(obj, 'visible', {
-        get: function() {
-            console.log('获取对象属性');
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            return value;
         },
-        set: function() {
-            console.log('设置对象属性');
+        set: function(v) {
+            value = v;
+            console.log('设置对象属性', v);
         }
     });
 }
+watch(obj);
+obj.visible = true;
