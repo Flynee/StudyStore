6365c8a8e4b122ca4c5ac4ee9f3e631aaebab586
Flynee <eeeezxc@163.com>
2020-07-31 15:23:33 +0800

    feat: 添加同源请求

--- CORS请求/app.js
+++ CORS请求/app.js
@@ -1,13 +1,19 @@
 var http = require('http');
 var url = require('url');
-var querystring = require('querystring');
 
+// 简单请求
 http.createServer(function(req, res) {
     var URL = url.parse(req.url);
-    var params = querystring.parse(URL.query);
+    // 简单请求，响应头必须设置允许跨域的源
     res.setHeader("Access-Control-Allow-Origin", "*"); 
+    // 复杂请求,响应头除了上面，还必须设置Access-Control-Allow-Methods
+    res.setHeader("Access-Control-Allow-Methods", "GET,PUT,DELETE,POST,OPTIONS"); 
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Control-Request-Method"); 
+    res.setHeader("Access-Control-Max-Age", 5); 
+
+    console.log(req.method);
     if (URL.pathname === '/test') {
-        res.end('hello world:)');
+        res.end(JSON.stringify({"say":"hello world:)"}));
     }
 }).listen(3000, (err) => {   
     if (!err) {
